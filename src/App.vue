a71a5365701ba807b0ddf2ef8a0e806f18d3657d
Elliot <braemelliot@gmail.com>
2018-07-10 11:45:49 -0300

    working vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template lang="pug">
   #app
-    h1 Working     
+    b-container
+      b-jumbotron(header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2")
+        p For more information visit our website
+        b-btn(variant="primary" href="https://bootstrap-vue.js.org/") More Info
+
+      b-form-group( horizontal :label-cols="4" description="Let us know your name." label="Enter your name")
+        b-form-input(v-model.trim="name")
+
+      b-alert(variant="success") Hello {{ name }}    
 </template>
 
 <script>
