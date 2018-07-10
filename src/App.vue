afa0c563d928dd3f4dd1b318e918f93716246588
Elliot <braemelliot@gmail.com>
2018-07-10 13:13:20 -0300

    outline of page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,53 @@
 <template lang="pug">
-  #app
-    b-container
-      b-jumbotron(header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2")
-        p For more information visit our website
-        b-btn(variant="primary" href="https://bootstrap-vue.js.org/") More Info
+  b-container
+    //- Navbar
+    nav
+      b-navbar(variant="faded" type="light")
+        b-navbar-brand.mb-0(tag="h1") Peekaboo Pediatric Therapy LLC
+        a(href="#" target="_blank")
+          i.fa.fa-facebook
+    //- Header
+    header
+      b-container
+        h1 Peekaboo Pediatric Therapy LLC
+    //- Main content
+    main
+      //- Services
+      section
+        b-container
+          b-card.mb-2(title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 18rem; display: inline-block;")
+            p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
+          b-card.mb-2(title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 18rem; display: inline-block;")
+            p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
+          b-card.mb-2(title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 18rem; display: inline-block;")
+            p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
+        b-container
+          b-jumbotron(header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2")
+            p For more information visit our website
+            b-btn(variant="primary" href="https://bootstrap-vue.js.org/") More Info
+      //- Rates
+      section
 
-      b-form-group( horizontal :label-cols="4" description="Let us know your name." label="Enter your name")
-        b-form-input(v-model.trim="name")
+      //- About us
+      section
 
-      b-alert(variant="success") Hello {{ name }}    
+    footer
+ 
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/common.scss'
 
 Vue.use(VueRouter)
