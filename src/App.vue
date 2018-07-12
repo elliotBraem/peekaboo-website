c9c9ab63ea93c52a4f6e7abbae6a5ce40d5cf1b5
Elliot <braemelliot@gmail.com>
2018-07-12 18:35:19 -0300

    services provider by buttons

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
       //- Services
       section.services
         .title-container
-          h1 Services
+          h2 Services
         b-container
           .row
             .col
@@ -40,21 +40,19 @@
                 tag="article"
                 style="max-width: 18rem; display: inline-block;")
                 p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
-            .col
-              b-card.mb-2(title="Card Title"
-                img-src="http://via.placeholder.com/380x400"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 18rem; display: inline-block;")
-                p.card-text Some quick example text to build on the card title and make up the bulk of the card's content.
       //- Rates
       section.rates
-        h1 Our Rates
+        h2 Services are provided through
+        b-button-group(vertical)
+          b-button(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
+          b-button(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
+          b-button(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
+
+        h2 Our Rates
 
       //- About us
       section.about
-        h1 About Us
+        h2 About Us
 
     footer
  
