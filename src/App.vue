a106e0db79e9e68a88b172d6fc0c344fa403d56d
Elliot <braemelliot@gmail.com>
2018-07-17 16:07:56 -0300

    resolved conflicts

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,8 @@
                 i.fa.fa-envelope
                 | Email
         //- Logo
-<<<<<<< HEAD
         img.logo(src="./assets/img/logo.svg" alt="Peekaboo Pediatric Therapy LLC")
         //h5.text-right Occupational Therapy Office in Denver, CO
-=======
-        .logo
-        // h5.text-right Occupational Therapy Office in Denver, CO
->>>>>>> d54f1c37a673a5c27dec177bc43330c2e8f35846
     //- Main content
     main
       // section.landing-contianer
@@ -34,7 +29,6 @@
           h2 Welcome to Peek-a-boo Pediatric Therapy LLC!
           h4(font-type: ) We're a Occupational Therapy office in the Denver area. We offer treatment to 
             | Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor, metus eget egestas ornare, quam quam gravida magna, quis rutrum nunc quam id sem. Praesent tristique bibendum leo vitae iaculis. Sed arcu enim, interdum pellentesque tincidunt eget, tristique in risus. Aliquam quis facilisis risus, a efficitur nisl. Nullam maximus maximus consequat. Quisque non dolor eu lorem finibus facilisis et ultrices nunc. Quisque in porttitor ipsum. Mauris rhoncus in elit ut scelerisque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-<<<<<<< HEAD
         img.child(src="./assets/img/girl-pink-hair.svg" alt="Girl with pink hair" fluid)
       //- Rates
       section.rates
@@ -44,17 +38,6 @@
           .btn(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
           .btn(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
 
-=======
-        b-img.child(src="./assets/img/girl-pink-hair.svg" alt="Girl with pink hair" fluid)
-        b-container.text-center
-          h2 We provide services through
-          b-button-group(vertical)
-            b-button(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
-            b-button(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
-            b-button(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
-      //- Rates
-      section.rates
->>>>>>> d54f1c37a673a5c27dec177bc43330c2e8f35846
         .title-container
           h2 Our Rates
         h2 We accept
@@ -76,10 +59,6 @@
 
 <script>
 export default {
-<<<<<<< HEAD
   name: 'app',
-=======
-  name: 'app'
->>>>>>> d54f1c37a673a5c27dec177bc43330c2e8f35846
 }
 </script>
