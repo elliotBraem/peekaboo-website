fea8b2b2bc8c93ff765bfd6cde1837efc6fcbd25
Elliot <braemelliot@gmail.com>
2018-07-17 16:44:41 -0300

    added about us outline

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
     //- Main content
     main
       // section.landing-contianer
-      //   b-img.landing-image(src="./assets/img/kid-painted-hands.jpeg" alt="Happy kid with painted hands" fluid)
+      //   b-img.landing-image(src="./assets/img/kid-painted-hands.jpeg" alt="Happy kid with painted hands")
       //- Services
       section.services
         // .title-container
@@ -29,14 +29,14 @@
           h2 Welcome to Peek-a-boo Pediatric Therapy LLC!
           h4(font-type: ) We're a Occupational Therapy office in the Denver area. We offer treatment to 
             | Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor, metus eget egestas ornare, quam quam gravida magna, quis rutrum nunc quam id sem. Praesent tristique bibendum leo vitae iaculis. Sed arcu enim, interdum pellentesque tincidunt eget, tristique in risus. Aliquam quis facilisis risus, a efficitur nisl. Nullam maximus maximus consequat. Quisque non dolor eu lorem finibus facilisis et ultrices nunc. Quisque in porttitor ipsum. Mauris rhoncus in elit ut scelerisque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        img.child(src="./assets/img/girl-pink-hair.svg" alt="Girl with pink hair" fluid)
+        img.child(src="./assets/img/girl-pink-hair.svg" alt="Girl with pink hair")
       //- Rates
       section.rates
         h2 Services are provided through
         .btn-group-vertical
-          .btn(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
-          .btn(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
-          .btn(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
+          button.btn(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
+          button.btn(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
+          button.btn(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
 
         .title-container
           h2 Our Rates
@@ -48,9 +48,31 @@
         .title-container
           h2 Meet the Therapists!
         .container
-          .card
+          .card.employee
+            .card-img-top(src="./assets/img/employees/amanda.jpeg" alt="Amanda Shimkus")
             .card-body
-              h1 Hello
+              h3.card-title Amanda Shimkus
+              .card-text Bio about Amanda
+              a.card-link(href="#") Read more!
+          .card.employee
+            .card-img-top(src="./assets/img/employees/lindsey.jpeg" alt="Lindsey Blanke")
+            .card-body
+              h3.card-title Lindsey Blanke
+              .card-text Bio about Lindsey
+              a.card-link(href="#") Read more!
+          .card.employee
+            .card-img-top(src="./assets/img/employees/nicole.jpeg" alt="Nicole Green")
+            .card-body
+              h3.card-title Nicole Green
+              .card-text Bio about Nicole
+              a.card-link(href="#") Read more!
+          .card.employee
+            .card-img-top(src="./assets/img/employees/amanda.jpeg" alt="Libby Cronin")
+            .card-body
+              h3.card-title Libby Cronin
+              .card-text Bio about Libby
+              a.card-link(href="#") Read more!
+            
 
 
     footer
