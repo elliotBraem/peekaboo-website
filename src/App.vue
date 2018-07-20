86a45df688ff8328978dce4827d5e47e1b968e6e
Elliot <braemelliot@gmail.com>
2018-07-20 00:32:54 -0300

    Rates layout and design

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
           // | Occupations change as a child develops. 
           // | An occupational therapist is an expert in all aspects of development. 
           h4.text-center Our therapists are experts in all aspects of your child's development. 
-          div.how-list
+          div.blue-box
             h5 We look at the child in 
             ul.p-3
               li 
@@ -110,13 +110,23 @@
       section.rates
         img.child(style="float: left; margin-left: -60px;" src="./assets/img/children/boy-yellow-shirt.svg" alt="Boy with yellow shirt")
         .container.text-center
-          h2 Our Rates
-          h4.text-center We contract with the following community center boards:
-          .btn-group-vertical
-            a(href="https://www.dpcolo.org/our-programs/early-intervention-department/early-intervention-program/"): button.btn(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
-            a(href="https://www.rmhumanservices.org/ei"): button.btn(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
-            a(href="https://ddrcco.com/early-intervention/home"): button.btn(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
-
+          h2.pb-3 Our Rates
+          h4.text-center Early Intervention
+          h5.text-center We contract with the following community center boards:
+          .btn-group-vertical.regions
+            // a(href="https://www.dpcolo.org/our-programs/early-intervention-department/early-intervention-program/"): 
+            button.btn(style="background-image: url('https://www.dpcolo.org/wp-content/uploads/2018/01/DP-LOGO-V.png')") 
+            br
+            // a(href="https://www.rmhumanservices.org/ei"): 
+            button.btn(style="background-image: url('https://static.wixstatic.com/media/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png/v1/fill/w_530,h_152,al_c,usm_0.66_1.00_0.01/c74773_0a1ad8f5ede04319a34a5a499ad76d26~mv2_d_4243_1215_s_2.png')")
+            br
+            // a(href="https://ddrcco.com/early-intervention/home"): 
+            button.btn(style="background-image: url('https://ddrcco.com/images/brand/logo.png'")
+            br
+          div.blue-box
+            p.p-3 Early intervention is a state ran program for children that have minimal to moderate/severe delays in 1 or more areas of development. To see if your child qualifies you can call your local community center board intake coordinator to set up an initial evaluation. It is a no-cost to family program and in doing so they will attempt to bill insurance and medicaid. You will not have to worry about your deductible or co-pays through this program.
+          br
+          p If you wish to not go through the Early Intervention program we offer private pay rates. Please <a href="#">contact us</a> for more information.
       //- About us
       section.about
         .container
@@ -143,6 +153,12 @@
               img.float-left.headshot.pr-3(src="./assets/img/employees/libby-headshot.jpg" alt="Libby Cronin")
               h3.card-title Libby Cronin
               .card-text Bio about Libby
+              a.card-link(href="#") Read more!
+          .card.employee
+            .card-body
+              img.float-left.headshot.pr-3(src="http://via.placeholder.com/170x170" alt="Lara Dato")
+              h3.card-title Lara Dato
+              .card-text Bio about Lara
               a.card-link(href="#") Read more!
             
 
