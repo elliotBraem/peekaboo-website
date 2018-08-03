069931e1fd37133c641ce0a11a4b4785332da09a
elliotBraem <braemelliot@gmail.com>
2018-08-03 16:56:36 -0500

    try catch

--- server/index.js
+++ server/index.js
@@ -6,27 +6,26 @@ const debug = require('debug')('app')
 
 const isDev = (process.env.NODE_ENV || 'development') === 'development'
 const app = express()
-
-if (isDev) {
-  const webpack = require('webpack')
-  const webpackConfig = require('../webpack.config')
-  const compiler = webpack(webpackConfig)
-
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath
-  }))
-  app.use(require('webpack-hot-middleware')(compiler))
-}
-
-app.use(compression());
-app.use(helmet());
-app.set('view engine', 'pug')
-app.set('views', path.resolve(__dirname, 'views'))
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
 try {
+  if (isDev) {
+    const webpack = require('webpack')
+    const webpackConfig = require('../webpack.config')
+    const compiler = webpack(webpackConfig)
+
+    app.use(require('webpack-dev-middleware')(compiler, {
+      noInfo: true, publicPath: webpackConfig.output.publicPath
+    }))
+    app.use(require('webpack-hot-middleware')(compiler))
+  }
+
+  app.use(compression());
+  app.use(helmet());
+  app.set('view engine', 'pug')
+  app.set('views', path.resolve(__dirname, 'views'))
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
   app.listen(8080, () => console.log('Server running!'))
 } catch(err) {
   debug('Error: %o', err)
