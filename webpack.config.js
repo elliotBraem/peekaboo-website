792576c634cb908a552bbf7f78d1002de05a7abd
elliotBraem <braemelliot@gmail.com>
2018-08-06 12:34:27 -0500

    minimize false

--- webpack.config.js
+++ webpack.config.js
@@ -108,19 +108,20 @@ module.exports = {
     hints: false
   },
   optimization: {
-    minimizer: [
-      // we specify a custom UglifyJsPlugin here to get source maps in production
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true
-        },
-        sourceMap: true
-      })
-    ]
+    minimize: false
+    // minimizer: [
+    //   // we specify a custom UglifyJsPlugin here to get source maps in production
+    //   new UglifyJsPlugin({
+    //     cache: true,
+    //     parallel: true,
+    //     uglifyOptions: {
+    //       compress: false,
+    //       ecma: 6,
+    //       mangle: true
+    //     },
+    //     sourceMap: true
+    //   })
+    // ]
   },
   devtool: '#eval-source-map'
 }
