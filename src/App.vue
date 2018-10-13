0768d08927e87e4ba9941b89b5f576dbd410e41b
elliotBraem <braemelliot@gmail.com>
2018-10-12 19:44:36 -0500

    feat: moves fonts.styl to css and removes font from App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,12 @@
 </template>
 
 <style lang="stylus" scoped>
+  // // /** Fonts */
+  // @font-face
+  //   font-family "Marker-Felt"
+  //   src url("static/fonts/Marker-Felt/Marker-Feltfont.ttf.woff") format("woff"), url("static/fonts/Marker-Felt/Marker-Feltfont.ttf.svg#Marker-Felt") format("svg"), url("static/fonts/Marker-Felt/Marker-Feltfont.ttf.eot"), url("static/fonts/Marker-Felt/Marker-Feltfont.ttf.eot?#iefix") format("embedded-opentype")
+  //   font-weight normal
+  //   font-style normal
   .container
     font-family 'Marker-Felt', sans-serif
     max-width 1200px
