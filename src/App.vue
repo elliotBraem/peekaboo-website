68813910758a61a9ef2d259a95e8d1bca9adb700
elliotBraem <braemelliot@gmail.com>
2018-10-14 15:08:54 -0500

    feat: chore: splits homepage into seperate Vue components

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,10 @@
       .container
         //- Navbar + Social Media
         nav.navbar.justify-content-end.pt-3
-            a.nav-item.pr-2(href="https://www.facebook.com/peekaboopediatrictherapy" target="_blank")
-              i.fa.fa-facebook-square
-                | Follow us on Facebook!
+            a.nav-item(href="https://www.facebook.com/peekaboopediatrictherapy" target="_blank")
+              span
+                i.fa.fa-facebook-square.d-inline
+                h6.d-inline.pl-2 Follow us on Facebook!
             // a.nav-item.pl-2.pr-2(href="#" target="_blank")
             //   i.fa.fa-envelope
             //   | Email
@@ -21,3 +22,8 @@
     .container-fluid
       router-view
 </template>
+
+<style lang="stylus" scoped>
+  i
+    font-size: 1.5em;
+</style>
---
+++ src/components/Homepage/WelcomeSummary.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    .summary-child
+        div.helper-div
+        img.child(src="static/img/children/girl-pink-hair.svg")
+        .container.text-center
+            h2.pb-5.pt-3 Welcome to Peek-a-boo Pediatric Therapy LLC!
+            h4 Peek-A-Boo Pediatric Therapy provides occupational therapy services to children birth to 3 years old and their families in the Denver-metro area.
+            br
+            h4 We provide services in environments where your child experiences the most; your home, your daycare or in the community.
+            br
+            h4 We are named Peek-A-Boo Pediatric Therapy because every child should have the opportunity to play the game of peek-a-boo with their parents, siblings, grandparents and any other person in their life.
+</template>
