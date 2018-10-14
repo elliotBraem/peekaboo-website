a405f429d3cdef8e342c81620c0a1d62396f4c8c
elliotBraem <braemelliot@gmail.com>
2018-10-14 14:19:36 -0500

    feat: changes facebook link in nav bar

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         nav.navbar.justify-content-end.pt-3
             a.nav-item.pr-2(href="https://www.facebook.com/peekaboopediatrictherapy" target="_blank")
               i.fa.fa-facebook-square
-              | Facebook
+                | Follow us on Facebook!
             // a.nav-item.pl-2.pr-2(href="#" target="_blank")
             //   i.fa.fa-envelope
             //   | Email
