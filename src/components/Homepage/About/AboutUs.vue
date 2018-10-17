5cf2ab0f6cf2eaf1cb7494a8e3b648ae9dd8bed3
elliotBraem <braemelliot@gmail.com>
2018-10-16 22:04:46 -0500

    feat: reformats employee cards

--- src/components/Homepage/About/AboutUs.vue
+++ src/components/Homepage/About/AboutUs.vue
@@ -1,76 +1,31 @@
 <template lang="pug">
-  .container
+  .container-fluid
     h2 Meet the <br> Therapists
     div.about-child
       div.helper-div
       img.child(src="static/img/children/girl-apple.svg")
-      .card.employee
-        .card-body
-          img.float-left.headshot.pr-3(src="static/img/employees/amanda-headshot.jpg" alt="Amanda Shimkus")
-          h3.card-title Amanda Shimkus
-          .card-text
-            h6.text-muted MA, OTR/L SIPT Certified
-            h6.job-title Owner, Occupational Therapist
-          a.card-link(href="#") Read about me!
-      br
-      .card.employee
-        .card-body
-          img.float-left.headshot.pr-3(src="static/img/employees/lindsey-headshot.jpg" alt="Lindsey Blanke")
-          h3.card-title Lindsey Blanke
-          .card-text
-            h6.text-muted OTR/L
-            h6.job-title Occupational Therapist
-          a.card-link(href="#") Read about me!
-      br
-      .card.employee
-        .card-body
-          img.float-left.headshot.pr-3(src="static/img/employees/nicole-headshot.jpg" alt="Nicole Green")
-          h3.card-title Nicole Green
-          .card-text
-            h6.text-muted OTD, OTR/L
-            h6.job-title Occupational Therapist
-          a.card-link(href="#") Read about me!
-      br
-      .card.employee
-        .card-body
-          img.float-left.headshot.pr-3(src="static/img/employees/libby-headshot.jpg" alt="Libby Cronin")
-          h3.card-title Libby Cronin
-          .card-text
-            h6.text-muted MS, OTR/L
-            h6.job-title Occupational Therapist
-          a.card-link(href="#") Read about me!
-      br
-      .card.employee
-        .card-body
-          img.float-left.headshot.pr-3(src="static/img/employees/lara-headshot.png" alt="Lara Dato")
-          h3.card-title Lara Dato
-          .card-text
-            h6.text-muted MS, OTR/L
-            h6.job-title Occupational Therapist
-          a.card-link(href="#") Read about me!
-      br
+      .row.justify-content-center
+        EmployeeCard.col-sm(name="Amanda Shimkus" headshot="static/img/employees/amanda-headshot.jpg" credential="MA, OTR/L SIPT Certified" title="Owner, Occupational Therapist" link="/amanda-shimkus")
+        EmployeeCard.col-sm(name="Lindsey Blanke" headshot="static/img/employees/lindsey-headshot.jpg" credential="OTR/L" title="Occupational Therapist" link="/lindsey-blanke")
+        EmployeeCard.col-sm(name="Nicole Green" headshot="static/img/employees/nicole-headshot.jpg" credential="OTD, OTR/L" title="Occupational Therapist" link="/nicole-green")
+      .row.justify-content-center
+        EmployeeCard.col-sm(name="Libby Cronin" headshot="static/img/employees/libby-headshot.jpg" credential="MS, OTR/L" title="Occupational Therapist" link="/libby-cronin")
+        EmployeeCard.col-sm(name="Lara Dato" headshot="static/img/employees/lara-headshot.png" credential="MS, OTR/L" title="Occupational Therapist" link="/lara-dato")
 </template>
+
+<script>
+import EmployeeCard from './EmployeeCard'
+
+export default {
+  components: {
+    EmployeeCard
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 .about h2
   padding-left: 1rem
-
-.about h3
-  font-size: 1.2rem
-
-.card
-  border: none
-  -webkit-box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
-  -moz-box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
-  box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
-
-.headshot
-  max-width: 170px
-  border-radius: 50%
-
-.job-title
-  padding-top: 0.5rem
-  clear: both
 
 .about-child
   position: relative
@@ -78,12 +33,12 @@
   height: auto
 
 .about-child .helper-div
-  margin-bottom: 200px
+  margin-bottom: 170px
   width: auto
 
 .about-child .child
   position: absolute
   z-index: -10
-  top: -175px
+  top: -160px
   right: -30px
 </style>
---
+++ src/components/Homepage/About/EmployeeCard.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  .card.m-3.employee
+    .card-body.text-center
+      img.card-img-top.headshot.pr-3(:src="headshot" :alt="name")
+      h3.pt-2.card-title {{ name }}
+      .card-text
+        h6.text-muted {{ credential }}
+        h6.job-title {{ title }}
+      router-link(:to="link").card-link Read about me!
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    headshot: {
+      type: String,
+      required: true
+    },
+    credential: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+h3
+  font-size: 1.2rem
+
+.card
+  max-width: 18rem
+  border: none
+  -webkit-box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
+  -moz-box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
+  box-shadow: 9px 6px 19px 0px rgba(0, 0, 0, 0.3)
+
+.headshot
+  border-radius: 50%
+
+.job-title
+  padding-top: 0.5rem
+  clear: both
+</style>
