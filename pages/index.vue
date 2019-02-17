fc005bb6cee56b358bd97235db4190128c983860
elliotBraem <braemelliot@gmail.com>
2019-02-17 13:16:45 -0600

    refactor: organization for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,26 @@
-<template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        peekaboo-website
-      </h1>
-      <h2 class="subtitle">
-        Official website for Peekaboo PEdiatric Therapy LLC in Denver, CO
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  section.container
+    div
+      h1.title peekaboo-website
+      h2.subtitle Official website for Peekaboo Pediatric Therapy LLC in Denver, CO
+      Welcome
+      Services
+      Rates
+      About
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Welcome from '~/pages/Welcome.vue'
+import Services from '~/pages/Services.vue'
+import Rates from '~/pages/Rates.vue'
+import About from '~/pages/About.vue'
 
 export default {
   components: {
-    Logo
+    Welcome,
+    Services,
+    Rates,
+    About
   }
 }
 </script>
