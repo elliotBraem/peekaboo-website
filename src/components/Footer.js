3a43548be2d54c2b511125b7de508c92d25f29f6
Elliot Braem <elliot@brae.me>
2020-11-24 17:30:07 -0600

    feat: updates to peekaboo logo, removes vimeo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -93,13 +92,6 @@ const Footer = class extends React.Component {
                   <img
                     src={instagram}
                     alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
