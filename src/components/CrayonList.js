c250b04f0c178f8e68ae2b22e3b64ff9fad6e4ae
Elliot Braem <elliot@brae.me>
2021-01-16 19:00:09 -0600

    feat: adds crayon list (WIP)

--- src/components/CrayonList.js
+++ src/components/CrayonList.js
@@ -1,41 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import blueCrayon from '../img/crayons/blue-crayon.svg'
+import greenCrayon from '../img/crayons/green-crayon.svg'
+import orangeCrayon from '../img/crayons/orange-crayon.svg'
+import purpleCrayon from '../img/crayons/purple-crayon.svg'
+import redCrayon from '../img/crayons/red-crayon.svg'
+import yellowCrayon from '../img/crayons/yellow-crayon.svg'
 
-const CrayonList = ({ listItems }) => (
-  <div className="columns is-multiline">
-    {listItems.map((item) => (
-      <div key={item} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <Crayon text={item} />
-              <PreviewCompatibleImage imageInfo={item} />
-            </div>
-          </div>
-        </section>
-      </div>
-    ))}
-  </div>
+const CrayonList = ({ treatments }) => (
+  <div className="columns is-multiline crayon-list-container">
+    {
+      treatments.map((treatment, index) => (
+        <div key={treatment} className="column is-half">
+          <Crayon text={treatment} img={getCrayonImg()} isFlipped={index % 2 !== 0} />
+        </div>
+      ))
+    }
+  </div >
 )
 
-CrayonList.propTypes = {
-  listItems: PropTypes.arrayOf(PropTypes.string)
+const getCrayonImg = () => {
+  const crayonImgs = [
+    blueCrayon,
+    greenCrayon,
+    orangeCrayon,
+    purpleCrayon,
+    redCrayon,
+    yellowCrayon
+  ];
+  return crayonImgs[Math.floor(Math.random() * crayonImgs.length)];
 }
 
-const Crayon = ({ text }) => {
-  return (<div>
-    <PreviewCompatibleImage imageInfo={text} />
-  </div>)
+CrayonList.propTypes = {
+  treatments: PropTypes.arrayOf(PropTypes.string)
+}
+
+CrayonList.defaultProps = {
+  treatments: []
+}
+
+const Crayon = ({ text, isFlipped }) => {
+  return (
+    <div className="crayon-container">
+      <img src={getCrayonImg()} alt="Child" style={isFlipped ? { transform: 'rotate(180deg)' } : null} />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 Crayon.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  isFlipped: PropTypes.bool
 }
 
 export default CrayonList
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import child from '../img/children/boy-red-shirt.svg'
+import redShirt from '../img/children/boy-red-shirt.svg'
+import blackHair from '../img/children/girl-black-hair.svg'
 
 import Layout from '../components/Layout'
+import CrayonList from '../components/CrayonList'
 
 export const IndexPageTemplate = ({
   image,
@@ -35,30 +37,58 @@ export const IndexPageTemplate = ({
     <div class="wave-container">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bde5ff" fill-opacity="1" d="M0,128L48,117.3C96,107,192,85,288,80C384,75,480,85,576,128C672,171,768,245,864,266.7C960,288,1056,256,1152,213.3C1248,171,1344,117,1392,90.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       <div class="behind-container">
-        <img src={child} alt="Child" className="child red-shirt" />
+        <img src={redShirt} alt="Child" className="child red-shirt" />
       </div>
     </div>
     {/* end mainpitch */}
     {/* start services */}
-    <section className="section section--gradient blue-background-color front-container">
+    <section className="section section--gradient blue-background-color">
       <div className="container">
         <div className="section">
-          <div className="column is-mobile is-centered">
-            <div className="column is-6">
-              <div className="content has-text-centered">
-                <h1 className="title">{services.heading}</h1>
-                <div className="tile">
-                  <h6 className="subtitle">{services.subheading}</h6>
-                </div>
-                <div className="tile">
-                  <h6 className="subtitle">{services.intro}</h6>
+          <div className="columns">
+            <div className="column is-half is-offset-6-tablet">
+              <div className="content">
+                <div className="tile is-ancestor">
+                  <div className="tile is-vertical">
+                    <h4>We look at the child in how they</h4>
+                    <div className="white-box m-3 p-3">
+                      <h4>manage their body when happy, upset, nervous, etc.</h4>
+                    </div>
+                    <div className="white-box m-3 p-3">
+                      <h4>interact and react to different sensory experiences such as touch, taste, smell, sound, and what they look at.</h4>
+                    </div>
+                    <div className="white-box m-3 p-3">
+                      <h4>interpret how they move and understanding the components needed to play with toys and their caregivers.</h4>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <CrayonList treatments={[
+            'interoception',
+            'regulation',
+            'sensory processing',
+            'motor skills',
+            'play',
+            'social skills',
+            'speech and language',
+            'feeding',
+            'visual impairments',
+            'hearing impairments',
+            'assistive technology',
+            'movement and handling',
+            'trauma and attachment'
+          ]} />
         </div>
       </div>
     </section>
+    <div class="wave-container">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bde5ff" fill-opacity="1" d="M0,96L48,106.7C96,117,192,139,288,165.3C384,192,480,224,576,218.7C672,213,768,171,864,144C960,117,1056,107,1152,122.7C1248,139,1344,181,1392,202.7L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      <div class="front-container">
+        <img src={blackHair} alt="Child" className="child black-hair" />
+      </div>
+    </div>
     {/* end services */}
     {/* start rates */}
     <section className="section section--gradient">
@@ -67,10 +97,10 @@ export const IndexPageTemplate = ({
           <div className="columns">
             <div className="content">
               <h1 className="title">{rates.heading}</h1>
-              {/* <div className="tile">
+              {/* <div className="div">
                   <h3 className="subtitle">{rates.subheading}</h3>
                 </div>
-                <div className="tile">
+                <div className="div">
                   <h3 className="subtitle">{rates.intro}</h3>
                 </div> */}
             </div>
@@ -81,16 +111,16 @@ export const IndexPageTemplate = ({
     {/* <div className="content has-text-centered">
                     <h1 className="title">{rates.heading}</h1>
                     <br />
-                    <div className="tile">
+                    <div className="div">
                       <h4 className="title">{rates.ei}</h4>
                       <h5 className="subtitle">{rates.subheading}</h5>
                     </div>
                     <br />
-                    <div className="tile">
+                    <div className="div">
                       <h3 className="subtitle">{mainpitch.line2}</h3>
                     </div>
                     <br />
-                    <div className="tile">
+                    <div className="div">
                       <h3 className="subtitle">{mainpitch.line3}</h3>
                     </div>
                   </div> */}
@@ -168,38 +198,38 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
+        markdownRemark(frontmatter: {templateKey: {eq: "index-page" } }) {
+        frontmatter {
         title
         image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
+        childImageSharp {
+        fluid(maxWidth: 2048, quality: 100) {
+        ...GatsbyImageSharpFluid
+      }
           }
         }
         heading
         subheading
         mainpitch {
-          heading
+        heading
           line1
           line2
           line3
         }
         services {
-          heading
+        heading
           subheading
           intro
           list
         }
         rates {
-          heading
+        heading
           ei
           subheading
         }
         description
         intro {
-          heading
+        heading
           description
         }
       }
